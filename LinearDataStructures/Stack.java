6034743b71c90a9db1aee14d621253bcee837ec9
salma570 <111149936+salma570@users.noreply.github.com>
2024-07-07 00:12:00 +0300

    Update Stack.java

--- LinearDataStructures/Stack.java
+++ LinearDataStructures/Stack.java
@@ -2,8 +2,25 @@ public class Stack{
   public static void main (String[] args){
     
     
-    Stack<Integer> st = new Stack<>();
+    Stack<Integer> st = new Stack<>(); 
+    // can add to size in bracket (2) eg.
+    //write all types complete 
+    //the fist character kman majuscule
+//add mubers to the stack 
+    st.push(6);
+    st.push(5);
+    st.push(4);
+    st.push(0);
+    st.push(9);
+    
+    st.pop(); //remove the top element 
+    st.peak(); // get the eleement on top
+    st.size(); //gets u the stack size
 
+    //remove all eleements
+    while(!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     
   }
 
