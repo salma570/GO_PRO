432a0018f31bc400ceda0785adffca4be0287f55
salma570 <111149936+salma570@users.noreply.github.com>
2024-07-07 23:57:28 +0300

    Update PriorityQueue.java

--- NonLinearDataStructures/PriorityQueue.java
+++ NonLinearDataStructures/PriorityQueue.java
@@ -2,6 +2,17 @@ public class PriorityQueue{
   public static void main(String [] args){
     PriorityQueue<Integer> pq = new PriorityQueue<>(); //queue FIFO
 
+    // is a queue in Ascending order by default
+    // elements could be repeated
+
+// For reversed order
+    PriorityQueue<Integer> pq1 = new PriorityQueue<>(Collections.reverseOrder()); //queue FIFO
+
+    // have all operations as the queue
+    // most operations' complexity  O(log(n))
+
+    
+
 
 
 
