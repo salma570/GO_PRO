40a378288f62b0cc1d23d2824feeee06cdb9e7e7
salma570 <111149936+salma570@users.noreply.github.com>
2024-07-14 11:49:25 +0300

    Create Angry_Monk.java

---
+++ Div3/Angry_Monk.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Angry_Monk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        int t=sc.nextInt();
+        while (t-->0) {
+            int k=sc.nextInt();
+            int n=sc.nextInt();
+            int[] a=new int[n];
+            for (int i=0;i<n;i++)
+                a[i]=sc.nextInt();
+            Arrays.sort(a);
+            long s=0;
+            for (int i=0;i<n-1;i++)
+                s+=(long)2*a[i]-1;
+            pw.println(s);
+ 
+        }
+ 
+        pw.flush();
+        }
+}
