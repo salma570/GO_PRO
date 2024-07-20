86f0598893055847a63a95d696a33703ddb22a9f
salma570 <111149936+salma570@users.noreply.github.com>
2024-07-20 19:07:13 +0300

    Update sort.java

--- SORTINGS/sort.java
+++ SORTINGS/sort.java
@@ -1,17 +1,18 @@
 public class sort{
+  
   public static void bubbleSort (int [] array){
-    
 /* type 1 bubble sort algorithm > swap each 2 adjacent cells 
 
 o(n^2)             max n: n^3/n^4
 traverse once (n operations) : 5 4 3 2 1 6
 traverse 2nd time (n operations) : 4 3 2 1 5 6
-
 */   
     int n = array.length;
     boolean flag = false;
+    
     for(int i =0; i<n ; i++){
       flag = true;
+      
       for(int j =0; j<n-1;j++){
         if(array[j+1]<array[j]){
           int temp = array[j+1];
@@ -19,12 +20,14 @@ traverse 2nd time (n operations) : 4 3 2 1 5 6
           array[j] = temp;
           flag = false;
         }
-        if(flag) break;
       }
+      if(flag) break;
     }
+  }
 
-
-
+ public static void bubbleSort (int [] array){
+   //o(nlog(n))         max n: n^5/n^6
+ }
 
 
 
@@ -33,7 +36,7 @@ traverse 2nd time (n operations) : 4 3 2 1 5 6
 
 
     
-    //o(nlog(n))         max n: n^5/n^6
+    
     //o(n)                max n: n^7/n^6
   }
 }
