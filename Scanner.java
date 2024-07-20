1707f19ad6431446cf9abe8b97f968a337ac43ab
salma570 <111149936+salma570@users.noreply.github.com>
2024-07-21 02:18:31 +0300

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class ContestScanner {
+	
+	public static class Scanner {
+		
+		StringTokenizer st;
+		BufferedReader br;
+
+		public Scanner(InputStream system) {
+			br = new BufferedReader(new InputStreamReader(system));
+		}
+
+		public Scanner(String file) throws Exception {
+			br = new BufferedReader(new FileReader(file));
+		}
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+
+		public String nextLine() throws IOException {
+			return br.readLine();
+		}
+
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		}
+
+		public boolean ready() throws IOException {
+			return br.ready();
+		}
+	}
+}
