5a77e6b451bbdc62c012446ba3f941c5fe912010
salma570 <111149936+salma570@users.noreply.github.com>
2024-07-20 19:36:41 +0300

    Update sort.java

--- SORTINGS/sort.java
+++ SORTINGS/sort.java
@@ -6,7 +6,7 @@ public class sort{
 public static void bubbleSort (int [] array){
 /* type 1 bubble sort algorithm > swap each 2 adjacent cells 
 
-o(n^2)             max n: n^3/n^4
+o(n^2)             max n: n^3
 traverse once (n operations) : 5 4 3 2 1 6
 traverse 2nd time (n operations) : 4 3 2 1 5 6
 */   
@@ -30,7 +30,7 @@ traverse 2nd time (n operations) : 4 3 2 1 5 6
 public static void selectionSort (int [] array){
    /*  2 selection sort algorithm > gets the smallest and sorts accordingly 
 n + (n-1)+ (n-2)+..
-o(n^2/2) = o(n^2)             max n: n^3/n^4
+o(n^2/2) = o(n^2)             max n: n^3
 traverse once (n operations) : 1 5 4 3 2 6
 traverse 2nd time (n operations) : 1 2 4 3 5 6
 */ 
@@ -51,15 +51,23 @@ traverse 2nd time (n operations) : 1 2 4 3 5 6
  }
 
 public static void InsertionSort (int [] array){
-/*  2 selection sort algorithm > gets the smallest and sorts accordingly 
-n + (n-1)+ (n-2)+..
-o(n^2/2) = o(n^2)             max n: n^3/n^4
-traverse once (n operations) : 1 5 4 3 2 6
-traverse 2nd time (n operations) : 1 2 4 3 5 6
+/*  2 selection sort algorithm > sorts tracversed array : select  
+the current elememnnt an dswitches it to the suitablel place  
+1 + 2+4+ ... +(n-1)
+o(n^2/2) = o(n^2)             max n: n^3
+traverse once (n operations) : 5 6 4 3 2 1
+traverse 2nd time (n operations) : 4 5 6 3 2 1
 */ 
-  
-  
-  
+ int n = array.length ;
+ 
+ for(int i=0; i<n;i++){
+  int temp = array[i]; j=i;
+  while(j>0&& array[j-1]>array[j]){
+      array[j] = array[j-1];
+      array[j-1] = temp;
+      j--; 
+  }
+ }
 }
   
   //o(nlog(n))         max n: n^5/n^6
